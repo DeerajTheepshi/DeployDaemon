5bff87d37ec9c58d3f86adcf136bbb18f5b108cd
Jayashrri <balaji.jayashrri@gmail.com>
2020-03-28 20:57:06 +0530

    Split run function into separate functions

--- app/php/composer.py
+++ app/php/composer.py
@@ -6,15 +6,26 @@ from app.apache.proxyPass import ProxyPass
 
 class Composer:
 
-    def run(self, serverName=None, port=None):
+    def setup(self):
         os.system('composer install')
         os.system('php artisan key:generate')
         os.system('php artisan migrate')
+
+    def seed(self):
         os.system('php artisan db:seed')
+
+    def start(self, serverName=None, port=None):
         runServer = 'php artisan serve' \
             + (' --host=' + serverName if serverName else '') \
             + (' --port=' + port if port else '')
         os.system(runServer)
+    
+    def restart(self, serverName+None, port=None):
+        stopServer = 'sudo kill $(sudo lsof -t' \
+            + (' -i:' + port if port else '8000') \
+            + ')'
+        os.system(stopServer)
+        self.start(serverName, port)
 
     def host(self, confName, port, serverName, serverAlias,
              documentRoot, route, urlToMap, priority):
